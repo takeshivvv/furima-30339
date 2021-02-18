c13f57329e686253e91b1e881290db5355a92696
takeshivvv <takeshi082608260826@gmail.com>
2021-02-18 14:59:28 +0900

    出品機能修正3

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -11,9 +11,7 @@ addTaxPrice.innerHTML = (Math.floor(inputValue * 0.1));
 
 const profitNumber = document.getElementById("profit")
 const value_result = inputValue * 0.1
-    console.log(value_result)
 profitNumber.innerHTML = (Math.floor(inputValue - value_result));
-  console.log(profitNumber);
 })
 
 });
