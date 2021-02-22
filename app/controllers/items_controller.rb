d5a44d83022ecbb6e79c73fc08b2eb0963e7bb37
takeshivvv <takeshi082608260826@gmail.com>
2021-02-22 15:08:45 +0900

    商品情報編集機能修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:edit, :show]
+  before_action :set_item, only: [:edit, :show, :update]
   
   def index
     @items = Item.includes(:user).order("created_at ASC")
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   end
 
     def update
-      item = Item.find(params[:id])
+      
       if item.update(item_params)
          redirect_to root_path
       else  
